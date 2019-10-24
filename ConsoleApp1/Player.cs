6505daa2c398314cd673372111e2d031ca6eee39
Aaron Walsh <S00189286@mail.itsligo.ie>
2019-10-24 12:44:54 +0100

    Add project files.

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Player : IComparable
+    {
+        public int ID { get; }
+        public string PlayerName { get; set; }
+        public int Score { get; private set; }
+
+        public static int HighScore { get; private set; }
+
+        public Player(int id, string playerName, int score)
+        {
+            ID = id;
+            PlayerName = playerName;
+            Score = score;
+        }
+
+        public void IncreaseScore(int valueToAdd)
+        {
+            if (Score < 100)
+                Score += valueToAdd;
+
+            if (Score > HighScore)
+                HighScore = Score;
+        }
+
+        public override string ToString()
+        {
+            return $"{ID}\t{PlayerName}\t{Score}"; 
+        }
+
+        public int CompareTo(object obj)
+        {
+            //get a reference to the next object in the list/array/collection
+            Player objectThatIAmComparingTo = (Player)obj;
+
+            //indicate what field I want to compare
+            int returnValue = this.Score.CompareTo(objectThatIAmComparingTo.Score);
+
+            //return
+            return returnValue;
+
+
+            //Player a = this;
+            //Player b = (Player)obj;
+
+            //int playerA_Score = a.Score;
+            //int playerB_Score = b.Score;
+
+
+            //int returnValue;
+
+            //if (playerA_Score > playerB_Score)
+            //    returnValue = 1;
+            //else if (playerA_Score < playerB_Score)
+            //    returnValue = -1;
+            //else returnValue = 0;
+
+            //return returnValue;
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1, "Millie", 0);
+            Player player2 = new Player(2, "Katie", 0);
+            Player player3 = new Player(3, "Kevin", 0);
+            Player player4 = new Player(4, "Conor", 0);
+            Player player5 = new Player(5, "Pauline", 0);
+
+            List<Player> allPlayers = new List<Player>();
+            allPlayers.Add(player1);
+            allPlayers.Add(player2);
+            allPlayers.Add(player3);
+            allPlayers.Add(player4);
+            allPlayers.Add(player5);
+
+            Display(allPlayers);
+            GetScores(allPlayers);
+            DisplayPlayerDetails(allPlayers);
+
+            allPlayers.Sort();
+            allPlayers.Reverse();
+            DisplayPlayerDetails(allPlayers);
+
+            Console.WriteLine($"The highest score is {Player.HighScore}");
+        }
+
+        private static void GetScores(List<Player> allPlayers)
+        {
+            Console.WriteLine("Please enter number of player you wish to add score for");
+            string response = Console.ReadLine();
+            int playerNumber = int.Parse(response);
+
+            while (playerNumber != 0)
+            {
+                //determine player selected
+                Player selectedPlayer = allPlayers.ElementAt(playerNumber - 1);//using -1 as index starts at 0
+
+                //increase score for that player
+                selectedPlayer.IncreaseScore(1);
+
+                //display results
+                Display(allPlayers);
+
+                //ask for another player or 0 to quit
+                Console.WriteLine("Please enter number of player you wish to add score for");
+                response = Console.ReadLine();
+                playerNumber = int.Parse(response);
+            }//end of while loop
+        }
+
+        private static void Display(List<Player> players)
+        {
+            Console.WriteLine("{0,-10}{1,-10}{2,-10}{3,-10}{4,-10}", "Player 1", "Player 2", "Player 3", "Player 4", "Player 5");
+
+            foreach (Player player in players)
+            {
+                Console.Write("{0,-10}", player.Score);
+            }
+
+            Console.WriteLine();//add a new line at the end of scores
+        }
+
+        private static void DisplayPlayerDetails(List<Player> players)
+        {
+            Console.WriteLine($"{"ID"}\t{"Name"}\t{"Score"}");
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
